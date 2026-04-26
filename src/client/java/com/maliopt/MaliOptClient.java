5cba82c2cba3a0b62b57754e906d5f576f00cb27
denotaravys <denotaravys@gmail.com>
2026-04-26 09:41:50 +0200

    Reorganizar estrutura: mover código cliente para src/client

---
+++ src/client/java/com/maliopt/MaliOptClient.java
@@ -0,0 +1,10 @@
+package com.maliopt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MaliOptClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // cliente
+    }
+}
